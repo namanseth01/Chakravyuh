274c50ff0bea5c1994cb3829c65ed3bc72ca83e9
namanseth01 <naman.seth_cs19@gla.ac.in>
2021-11-25 09:40:36 +0530

    edit

---
+++ js/signup.js
@@ -0,0 +1,41 @@
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from "https://www.gstatic.com/firebasejs/9.5.0/firebase-app.js";
+// import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.5.0/firebase-analytics.js";
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
+
+// // Your web app's Firebase configuration
+// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// const firebaseConfig = {
+//   apiKey: "AIzaSyCQmLSNPEz_i3Rwdd_DSXtMK4AqX5gmg5A",
+//   authDomain: "chakravyuh1-95927.firebaseapp.com",
+//   projectId: "chakravyuh1-95927",
+//   storageBucket: "chakravyuh1-95927.appspot.com",
+//   messagingSenderId: "62012441284",
+//   appId: "1:62012441284:web:15fe1987d498937753bf91",
+//   measurementId: "G-MPBTFJXJG5",
+// };
+
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
+
+// function signUp() {
+//   var email = document.getElementById("email");
+//   var password = document.getElementById("psw");
+
+//   const promise = auth.createUserWithEmailAndPassword(
+//     email.value,
+//     password.value
+//   );
+//   //
+//   promise.catch((e) => alert(e.message));
+//   alert("SignUp Successfully");
+// }
+
+// function signIn() {
+//   var email = document.getElementById("email");
+//   var password = document.getElementById("psw");
+//   const promise = auth.signInWithEmailAndPassword(email.value, password.value);
+//   promise.catch((e) => alert(e.message));
+// }
