65e4e91477d1ab4d57e79f458d1b7057335b58c6
namanseth01 <naman.seth_cs19@gla.ac.in>
2021-11-25 09:24:27 +0530

    formatted

--- js/question.js
+++ js/question.js
@@ -225,10 +225,9 @@ const questions = [
   },
 ];
 
-let shuffledQuestions = []; //empty array to hold shuffled selected questions
+let shuffledQuestions = [];
 
 function handleQuestions() {
-  //function to shuffle and push 10 questions to shuffledQuestions array
   while (shuffledQuestions.length <= 9) {
     const random = questions[Math.floor(Math.random() * questions.length)];
     if (!shuffledQuestions.includes(random)) {
@@ -242,12 +241,10 @@ let playerScore = 0;
 let wrongAttempt = 0;
 let indexNumber = 0;
 
-// function for displaying next question in the array to dom
 function NextQuestion(index) {
   handleQuestions();
   const currentQuestion = shuffledQuestions[index];
   document.getElementById("question-number").innerHTML = questionNumber;
-  //   document.getElementById("player-score").innerHTML = playerScore;
   document.getElementById("display-question").innerHTML =
     currentQuestion.question;
   document.getElementById("option-one-label").innerHTML =
@@ -261,19 +258,17 @@ function NextQuestion(index) {
 }
 
 function checkForAnswer() {
-  const currentQuestion = shuffledQuestions[indexNumber]; //gets current Question
-  const currentQuestionAnswer = currentQuestion.correctOption; //gets current Question's answer
-  const options = document.getElementsByName("option"); //gets all elements in dom with name of 'option' (in this the radio inputs)
+  const currentQuestion = shuffledQuestions[indexNumber];
+  const currentQuestionAnswer = currentQuestion.correctOption;
+  const options = document.getElementsByName("option");
   let correctOption = null;
 
   options.forEach((option) => {
     if (option.value === currentQuestionAnswer) {
-      //get's correct's radio input with correct answer
       correctOption = option.labels[0].id;
     }
   });
 
-  //checking to make sure a radio input has been checked or an option being chosen
   if (
     options[0].checked === false &&
     options[1].checked === false &&
@@ -283,13 +278,11 @@ function checkForAnswer() {
     document.getElementById("option-modal").style.display = "flex";
   }
 
-  //checking if checked radio button is same as answer
   options.forEach((option) => {
     if (option.checked === true && option.value === currentQuestionAnswer) {
       document.getElementById(correctOption).style.backgroundColor = "green";
       playerScore++;
       indexNumber++;
-      //set to delay question number till when next question loads
       setTimeout(() => {
         questionNumber++;
       }, 1000);
@@ -299,7 +292,6 @@ function checkForAnswer() {
       document.getElementById(correctOption).style.backgroundColor = "green";
       wrongAttempt++;
       indexNumber++;
-      //set to delay question number till when next question loads
       setTimeout(() => {
         questionNumber++;
       }, 1000);
@@ -307,11 +299,9 @@ function checkForAnswer() {
   });
 }
 
-//called when the next button is called
 function handleNextQuestion() {
   checkForAnswer();
   unCheckRadioButtons();
-  //delays next question displaying for a second
   setTimeout(() => {
     if (indexNumber <= 9) {
       NextQuestion(indexNumber);
@@ -322,7 +312,6 @@ function handleNextQuestion() {
   }, 1000);
 }
 
-//sets options background back to null after display the right/wrong colors
 function resetOptionBackground() {
   const options = document.getElementsByName("option");
   options.forEach((option) => {
@@ -330,7 +319,6 @@ function resetOptionBackground() {
   });
 }
 
-// unchecking all radio buttons for next question(can be done with map or foreach loop also)
 function unCheckRadioButtons() {
   const options = document.getElementsByName("option");
   for (let i = 0; i < options.length; i++) {
@@ -338,12 +326,10 @@ function unCheckRadioButtons() {
   }
 }
 
-// function for when all questions being answered
 function handleEndGame() {
   let remark = null;
   let remarkColor = null;
 
-  // condition check for player remark and remark color
   if (playerScore <= 3) {
     remark = "Bad Grades, Keep Practicing.";
     remarkColor = "red";
@@ -356,7 +342,6 @@ function handleEndGame() {
   }
   const playerGrade = (playerScore / 10) * 100;
 
-  //data to display to score board
   document.getElementById("remarks").innerHTML = remark;
   document.getElementById("remarks").style.color = remarkColor;
   document.getElementById("grade-percentage").innerHTML = playerGrade;
@@ -365,12 +350,10 @@ function handleEndGame() {
   document.getElementById("score-modal").style.display = "flex";
 }
 
-//closes score modal and resets game
 function closeScoreModal() {
   window.location.href = "../pages/home.html";
 }
 
-//function to close warning modal
 function closeOptionModal() {
   document.getElementById("option-modal").style.display = "none";
 }
